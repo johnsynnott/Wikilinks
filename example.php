d67ab2568f35cecfae5fae3f0e77abb29365d243
johnsynnott <johnsynn@gmail.com>
2013-10-05 17:11:12 -0400

    Allow page choosing

--- example.php
+++ example.php
@@ -1,5 +1,6 @@
 <?php
 	header("Content-Type: text/plain");
-	$raw = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Robert_H._Goddard&prop=links&pllimit=500');
+	$pages = $_GET['pages'];
+	$raw = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$pages.'&prop=links&pllimit=500');
 	print_r(json_decode($raw,true));
 ?>
