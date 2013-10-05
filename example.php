0919125f424b1f634887bc2d8b1847aaa9ad4bcd
johnsynnott <johnsynn@gmail.com>
2013-10-05 17:26:28 -0400

    I write terrible code

--- example.php
+++ example.php
@@ -1,6 +1,11 @@
 <?php
 	header("Content-Type: text/plain");
-	$pages = $_GET['pages'];
-	$raw = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$pages.'&prop=links&pllimit=500');
-	print_r(json_decode($raw,true));
+	$page1 = $_GET['page1'];
+	$page2 = $_GET['page2'];
+	$raw1 = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$page1.'&prop=links&pllimit=500');
+	$raw2 = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$page2.'&prop=links&pllimit=500');
+	$json1 = json_decode($raw1,true));
+	$json2 = json_decode($raw2,true));
+	print_r($json1);
+	print_r($json2);
 ?>
