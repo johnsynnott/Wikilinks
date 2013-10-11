3fc2f2ae02be083d7f06a051aab613a61d87b72e
johnsynnott <johnsynn@gmail.com>
2013-10-10 22:58:47 -0400

    Decided on Bootstrap 3, and started fiddling with page layout.

--- example.php
+++ example.php
@@ -1,11 +1,11 @@
 <?php
 	header("Content-Type: text/plain");
 	$page1 = $_GET['page1'];
-	$page2 = $_GET['page2'];
+	#$page2 = $_GET['page2'];
 	$raw1 = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$page1.'&prop=links&pllimit=500');
-	$raw2 = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$page2.'&prop=links&pllimit=500');
+	#$raw2 = file_get_contents('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='.$page2.'&prop=links&pllimit=500');
 	$json1 = json_decode($raw1,true));
-	$json2 = json_decode($raw2,true));
+	#$json2 = json_decode($raw2,true));
 	print_r($json1);
-	print_r($json2);
+	#print_r($json2);
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-echo "Hello World!\nI'm a placeholder! Nate was here...";
-?>
 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wikilinks</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
+</head>
+<body>
+<div class="header">
+    <h1>Wikilinks</h1>
+    <h2>And some info about it</h2>
+</div>
+<div class="pure-g-r">
+    <div class="pure-3-7">Inp
+</div>
+</body>
+</html>
